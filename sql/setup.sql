6c2945ef66f0d983c7ebed932e515acbeb8abdd0
BrendenSeifried <brendenseifried@gmail.com>
2022-06-14 10:11:56 -0700

    fixed issue with extra semi colon and turned dates into INT instead of Varchar

--- sql/setup.sql
+++ sql/setup.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS books;
 CREATE table books (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     title VARCHAR,
-    release VARCHAR
+    release INT
 );
 
 INSERT INTO books (
@@ -14,11 +14,12 @@ INSERT INTO books (
 )
 
 VALUES 
-('Poilu', 2015)
-('My Sisters Keeper', 2004)
-('War As I Knew It', 1947)
-('Never Split The Difference', 2016)
-('The Late Great Me', 1976)
-('The Notebook' 1996)
-('Fear And Loathing In Las Vegas', 1971)
+('Poilu', 2015),
+('My Sisters Keeper', 2004),
+('War As I Knew It', 1947),
+('Never Split The Difference', 2016),
+('The Late Great Me', 1976),
+('The Notebook', 1996),
+('Fear And Loathing In Las Vegas', 1971);
+
 
