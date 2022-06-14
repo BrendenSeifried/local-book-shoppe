2e6e60cb5face60db6b3fff7a4555bfa689b26cf
BrendenSeifried <brendenseifried@gmail.com>
2022-06-14 09:46:48 -0700

    complted book SQL and now shows up on beekeeper

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,17 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS books;
+
+CREATE table books (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title VARCHAR,
+    release VARCHAR
+);
+
+INSERT INTO books (
+    title,
+    release
+)
+
+VALUES 
+('Poilu', 2015)
