fe29267b2d5af99cf51c4e24f33d1c5a04181c9c
BrendenSeifried <brendenseifried@gmail.com>
2022-06-14 11:53:00 -0700

    first test made and passed for number of books

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,13 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('First test for Books', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('Checking that /books Renders a list of books.', async () => {
+    const resp = await request(app).get('/books');
+    expect(resp.body.length).toEqual(7);
   });
   afterAll(() => {
     pool.end();
