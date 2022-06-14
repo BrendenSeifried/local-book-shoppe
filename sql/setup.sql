581d00dffc10384361453d4f1a21686af5b03471
BrendenSeifried <brendenseifried@gmail.com>
2022-06-14 10:47:58 -0700

    authors now render to beekeeper with date of birth, moving to HTML next

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,7 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
 DROP TABLE IF EXISTS books;
+DROP TABLE IF EXISTS authors;
 
 CREATE table books (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -11,8 +12,8 @@ CREATE table books (
 CREATE TABLE authors (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     writer VARCHAR,
-    dob INT
-)
+    dob VARCHAR
+);
 
 INSERT INTO books (
     title,
@@ -29,12 +30,21 @@ VALUES
 ('Fear And Loathing In Las Vegas', 1971);
 
 
-INSERT INTO books (
+INSERT INTO authors (
     writer,
     dob
 )
 
 VALUES
-('Louis Barthas', 7.14.1879)
-('Hunter S Thompson', 7.18.1937)
+('Louis Barthas', '7.14.1879'),
+('Hunter S Thompson', '7.18.1937'),
+('Chris Voss', '11.28.1957'),
+('Tahl Raz', '4.15.1978'),
+('George S. Patton', '11.11.1885'),
+('Jodi Picoult', '5.19.1966'),
+('Nicholas Sparks', '12.31.1965'),
+('Sandra Scoppettone', '6.14.1936');
+
+
+
 
