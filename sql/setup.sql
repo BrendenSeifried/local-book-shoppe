4d45fd134f5f35a0a6e9bda5d97a32cd776e6f82
BrendenSeifried <brendenseifried@gmail.com>
2022-06-14 10:05:14 -0700

    Added a list of books to the SQL database.

--- sql/setup.sql
+++ sql/setup.sql
@@ -15,3 +15,10 @@ INSERT INTO books (
 
 VALUES 
 ('Poilu', 2015)
+('My Sisters Keeper', 2004)
+('War As I Knew It', 1947)
+('Never Split The Difference', 2016)
+('The Late Great Me', 1976)
+('The Notebook' 1996)
+('Fear And Loathing In Las Vegas', 1971)
+
